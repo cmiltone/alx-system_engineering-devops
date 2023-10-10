61e11da775f74d0480cda7563963d219be86fd3f
Castro Hamiltone <castromiltone@gmail.com>
2023-10-10 21:36:31 +0300

    codestyle fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    res= requests.get(url, headers=headers, allow_redirects=False)
+    res = requests.get(url, headers=headers, allow_redirects=False)
     if res.status_code == 404:
         return 0
     json = res.json().get("data")
